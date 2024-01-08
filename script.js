a76747d7b7f92e5501feb0425b3631758483bcb5
The Ace Developers <151608560+theacedevelopers@users.noreply.github.com>
2024-01-08 08:08:29 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
+};
+
+const sr = ScrollReveal({
+    distance: '40px',
+    duration: 2500,
+    reset: true
+});
+
+sr.reveal('.logo',{delay:200, origin:'left'});
+sr.reveal('.navbar',{delay:400, origin:'top'});
+sr.reveal('.menu-btn',{delay:500, origin:'right'});
+
